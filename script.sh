bfeac805b9e1730df2dd14bdde7c94fdcc78ca66
Larry Dong <larry.dong@mail.mcgill.ca>
2019-10-16 14:12:25 +0200

    Updated script

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-unzip data/M2PHDS_19-20_OMICS_TRANSC_MAARS_normTranscriptome_618samples_16042014.zip
-unzip filter-data/transcriptome.zip
+unzip data/M2PHDS_19-20_OMICS_TRANSC_MAARS_normTranscriptome_618samples_16042014.zip -d data/
+unzip filter-data/transcriptome.zip -d filter-data/
