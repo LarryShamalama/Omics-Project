82d1d7d4703e47b1a2405f224887f0b1e67599e1
Larry Dong <larry.dong@mail.mcgill.ca>
2019-10-17 19:53:11 +0200

    Finished prediction model template

--- analysis/utils.R
+++ analysis/utils.R
@@ -14,3 +14,56 @@ review_weights = function(cv_sl) {
   # Sort by decreasing mean weight.
   sl_stats[order(sl_stats[, 1], decreasing = TRUE), ]
 }
+
+
+roc.helper <- function(predictions, labels){
+  # returns coordinates for roc curve
+  stopifnot(length(predictions) == length(labels))
+  
+  n  <- length(predictions)
+  df <- cbind(predictions, lesion.status)
+  df <- df[order(df[,1], decreasing = TRUE),]
+  
+  roc.coord <- matrix(data=0, nrow=n, ncol=2)
+  
+  for (i in 1:n){
+    l <- as.vector(df[i,])[2]
+    
+    roc.coord[i,(l+1)] <- 1
+  }
+  
+  roc.coord <- apply(roc.coord, 2, cumsum)
+  stopifnot(roc.coord[n, 2] == sum(labels))
+  roc.coord[,1] <- roc.coord[,1]/roc.coord[n,1]
+  roc.coord[,2] <- roc.coord[,2]/roc.coord[n,2]
+  
+  roc.coord <- data.frame(roc.coord)
+  colnames(roc.coord) <- c('FPR', 'TPR')
+  
+  return (roc.coord)
+}
+
+plot.roc <- function(predictions, labels){
+  roc.coord <- roc.helper(predictions, labels)
+  n <- length(roc.coord[,1])
+  
+  library(ggplot2)
+  ggplot(data=roc.coord, aes(FPR, TPR)) +
+    geom_path() + 
+    labs(x='False Positive Rate (FPR)', 
+         y='True Positive Rate (TPR)') +
+    ggtitle(paste('ROC Curve\nArea under ROC =', round(sum(roc.coord$TPR)/n, digits = 3))) + 
+    theme(plot.title = element_text(hjust = 0.5))
+}
+
+
+get_sens_spec <- function(predictions, labels){
+  distance <- function(point){
+    return (sqrt(sum((point - c(1, 0))^2)))
+  }
+  
+  roc.coord <- roc.helper(predictions, labels)
+  all_distances <- apply(roc.coord, 1, distance)
+  best_coord <- all_distances == max(all_distances)
+  
+}
