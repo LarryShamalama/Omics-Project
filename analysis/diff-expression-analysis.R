72c9ece4c04406f00e50a2b35ae47a3d50e80498
Kristin <thewooz@Kristins-MacBook-Pro.local>
2019-10-26 06:03:46 +0200

    edits

--- analysis/diff-expression-analysis.R
+++ analysis/diff-expression-analysis.R
@@ -42,21 +42,32 @@ transcriptome <- t(transcriptome)
 samples$lesional_new[samples$lesional=="LES"]<- 1
 samples$lesional_new[samples$lesional=="NON_LES"]<- 0   
 
+library(gdata)
 # create the model matrix for paired data
 #design<- as.data.frame(model.matrix(~lesional_new, data=samples ))
-paired.design<- as.data.frame(model.matrix(~lesional_new + MAARS_identifier, data=samples))
-
+paired.design<- as.data.frame(model.matrix(~lesional_new + drop.levels(MAARS_identifier), data=samples))
 
 # fitting linear model    NOTE:  getting a warning message about NAs
 fit <- lmFit(transcriptome, design=paired.design)
 # eBayes makes the variance more flexible
 fit <- eBayes(fit, robust=TRUE)
 
-
 # significance test
 signif <- decideTests(fit, adjust.method = "BH", p.value = 0.05, lfc = 1)
 summary(signif)[ ,2]
 
+
+#### ignore all this
+# list of p-values for lesional/non-lesional per gene
+gene_pvalues<- as.data.frame(fit$p.value[,2])
+# calculate absolute value
+gene_pvalues$absvalue<- abs(gene_pvalues$lesional_new)
+# order by increasing abs value p-values
+gene_pvalues<- order(gene_pvalues, by=absvalue)
+####
+
+
 #write genes to a csv file
 write.fit(fit, signif, "myoutputdata.csv", adjust="BH")
+
 
